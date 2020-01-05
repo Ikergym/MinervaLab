8ce4c4cad14588caebaa9b606a81805fa8d26899
Jon Gabirondo López <jongablop@gmail.com>
2020-01-05 13:17:47 +0100

    Edit paths.

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('./apps/van_der_waals'))
+sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
