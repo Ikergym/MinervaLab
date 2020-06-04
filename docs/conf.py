dc712c29bb7c0aabc7f6beebfb31cb551a819493
Jon Gabirondo López <jongablop@gmail.com>
2020-06-04 12:27:29 +0200

    Test automatic equation numbering.

--- docs/conf.py
+++ docs/conf.py
@@ -35,6 +35,11 @@ extensions = [
     'sphinx.ext.mathjax',
 ]
 
+# Add automatic numbering on equations.
+mathjax_config = {
+    'TeX': {'equationNumbers': {'autoNumber': 'AMS', 'useLabelIds': True}},
+}
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
